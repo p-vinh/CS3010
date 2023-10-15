e020a2d980558a902fe5323191b9b5b946ce0fc7
random-debug <85326677+p-vinh@users.noreply.github.com>
2023-10-14 23:25:12 -0700

    Changes

--- gaussian.py
+++ gaussian.py
@@ -1,7 +1,30 @@
-import IOManager as io
 import sys
 import NaiveGaussElimination as nge
 import ScaledPartialPivot
+
+def readFile(file):
+    try:
+        coeff = []
+        const = []
+        with open(file, 'r') as f:
+            lines = f.readlines()
+            n = int(lines[0])
+            for i in range(1, len(lines)-1):
+                row = lines[i].split()
+                coeff.append(list(map(lambda row : float(row), row)))
+            lastRow = lines[len(lines)-1].split()
+            const = list(map(lambda lastRow : float(lastRow), lastRow))
+    except FileNotFoundError:
+        print("File not found!")
+        exit(0)
+    return n, coeff, const
+
+def saveOutput(fileName, sol):
+    with open(fileName, 'w') as f:
+        for i in range(len(sol)):
+            f.write("{:.15g} ".format(sol[i]))
+        f.write("\n")
+    print("Output saved to %s" %(fileName))
 
 def main(): 
     if len(sys.argv) < 2:
@@ -10,7 +33,7 @@ def main():
     
     spp = False
     name = sys.argv[-1]
-    n, coeff, const = io.readFile(name)
+    n, coeff, const = readFile(name)
     
     if len(sys.argv) == 3 and sys.argv[1] == "--spp":
         spp = True
@@ -26,7 +49,7 @@ def main():
         coeff, const = nge.FwdElimination(coeff, const, n)
         sol = nge.BackSubst(coeff, const, sol, n)
     name = name.replace(".lin", ".sol")
-    io.saveOutput(name, sol)
+    saveOutput(name, sol)
 
 
 if __name__ == '__main__':
